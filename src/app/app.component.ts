f67807f02e1285b3ebb27db82f25a00b6eabc93d
Kiranmai Chigarakula <kiranmaic14@gmail.com>
2024-05-26 13:57:43 -0500

    Added fuctionality for view and download resume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-portfolio';
-
   collapse = true;
+
   toggleCollapse(): void {
     this.collapse=!this.collapse;
+  }
+
+  viewResume() {
+    const pdfUrl = 'assets/kiranmai_chigarakula.pdf';
+    window.open(pdfUrl, '_blank');
   }
 }
