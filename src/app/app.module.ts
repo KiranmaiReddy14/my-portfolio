763037ee321db79b1458224f569a753514d87638
Kiranmai Chigarakula <kiranmaic14@gmail.com>
2025-02-14 12:05:26 -0500

    Added education details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { EducationComponent } from './education/education.component';
+import { WorkComponent } from './work/work.component';
 
 RouterModule.forRoot([
   { path: '', redirectTo: '#top', pathMatch: 'full' },
 ]);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EducationComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
